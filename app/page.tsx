9f38686dcdc0ced45039ffe9c4b6ca6134611bcc
MohammedAdnaanJ <mohamedadnan081@gmail.com>
2025-05-26 16:46:01 +0530

    Implement a Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+// import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to My SaaS App</h1>
-      <Button>Let's get Started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companion</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivative & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed Session"
+          companions={recentSessions}
+          className="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
