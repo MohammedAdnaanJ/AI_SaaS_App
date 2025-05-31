ce9c6341f87014a773b7ab69883cfc3322fdb348
MohammedAdnaanJ <mohamedadnan081@gmail.com>
2025-05-31 12:11:59 +0530

    Companion Form

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -11,8 +12,16 @@ const Navbar = () => {
           <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
         </div>
       </Link>
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-6">
         <NavItems />
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
